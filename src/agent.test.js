a5135047b61e958673460c4bd101197c5e5b2a7a
Erick Wendel <erick.workspace@gmail.com>
2020-02-20 12:33:37 -0300

    tests(): add test to verify test before agent
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- src/agent.test.js
+++ src/agent.test.js
@@ -1,4 +1,3 @@
-require('./agent').start()
 const { Server } = require('http')
 const { spy, assert } = require('sinon')
 const response = { 
@@ -7,12 +6,18 @@ const response = {
 
 const eventName = 'request'
 const expectedCallCount = 1
+const expectedZeroCalls = 0
 const request = null 
 
 
 const serverInstance = new Server()
 serverInstance.emit(eventName, request, response)
+assert.callCount(response.setHeader, expectedZeroCalls)
 
+
+// testing after binding the Agent
+require('./agent').start()
+serverInstance.emit(eventName, request, response)
 assert.callCount(response.setHeader, expectedCallCount)
 assert.calledWithExactly(response.setHeader, 'X-Instrumented-By', 'Sqreen')
 
